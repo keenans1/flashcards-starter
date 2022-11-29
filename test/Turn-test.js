69a6d109f3413b9eafb1e3e918df20f0a541e067
Keenan Southall <keenan.southall@yahoo.com>
2022-11-28 20:07:56 -0700

    Create Turn class and pass first test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -3,5 +3,10 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 
 describe('Turn', function () {
+    it('should be a function', function () {
 
+        const turn = new Turn();
+
+        expect(Turn).to.be.a('function');
+    })
 })

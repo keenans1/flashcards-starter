36339c77ed1ef34a52d8dfab742adf8fceb4fc0d
Keenan Southall <keenan.southall@yahoo.com>
2022-11-29 14:27:08 -0700

    Refactor Deck-test to use beforeEach

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -7,22 +7,18 @@ const prototypeData = require('../src/data.js')
 
 describe('Deck', function () {
 
-    // let deck;
-    // beforeEach(() = deck = new Deck(prototypeData));
+    let deck;
+    beforeEach(() => deck = new Deck(prototypeData));
 
     it('should be a function', function () {
         expect(Deck).to.be.a('function');
     })
 
     it('should contain all cards', function () {
-        const deck = new Deck(prototypeData);
-
         expect(deck.cards).to.deep.equal(prototypeData);
     })
 
     it('should count all cards', function () {
-        const deck = new Deck(prototypeData);
-
         const totalCards = deck.countCards();
 
         expect(totalCards).to.equal(30);

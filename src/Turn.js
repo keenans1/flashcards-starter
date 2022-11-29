c5e2b61aaf208491887ca6e287222d78ab678bfe
Keenan Southall <keenan.southall@yahoo.com>
2022-11-29 08:14:03 -0700

    Complete Turn class and pass all Turn tests

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,29 @@
 class Turn {
-    constructor() {
+    constructor(guess, card) {
+        this.guess = guess;
+        this.card = card;
+    }
 
+    returnGuess() {
+        return this.guess;
+    }
+
+    returnCard() {
+        return this.card;
+    }
+
+    evaluateGuess() {
+        if (this.guess === this.card.correctAnswer) {
+            return true;
+        }
+        return false;
+    }
+
+    giveFeedback() {
+        if (this.evaluateGuess()) {
+            return 'correct!';
+        }
+        return 'incorrect!';
     }
 }
 
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,12 +1,38 @@
 const chai = require('chai');
+const Card = require('../src/Card');
 const expect = chai.expect;
 const Turn = require('../src/Turn');
 
 describe('Turn', function () {
+    const card = new Card(1, 'How are you?', ['great', 'good', 'fine', 'bad'], 'good');
     let turn;
-    beforeEach(() => turn = new Turn());
+    beforeEach(() => turn = new Turn('good', card));
 
     it('should be a function', function () {
         expect(Turn).to.be.a('function');
+    })
+
+    it('should store the users guess', function () {
+        const userGuess = turn.returnGuess();
+
+        expect(userGuess).to.equal(card.correctAnswer)
+    })
+
+    it('should return the current turns card', function () {
+        const currentCard = turn.returnCard();
+
+        expect(currentCard).to.deep.equal(card);
+    })
+
+    it('should check if users guess is correct', function () {
+        const correct = turn.evaluateGuess();
+
+        expect(correct).to.equal(true);
+    })
+
+    it('should return feedback according to the players guess', function () {
+        const message = turn.giveFeedback();
+
+        expect(message).to.equal('correct!');
     })
 })

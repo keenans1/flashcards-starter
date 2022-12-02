4bc893cb5db90a3be28861f6fcf219ec227969aa
Keenan Southall <keenan.southall@yahoo.com>
2022-12-01 21:04:44 -0700

    Delete all comments

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 const Game = require("./src/Game");
 const startGame = new Game();
 startGame.start();
-
-// const count = 0;
-// const counter = () => console.log(count++);
-// setInterval(counter, 1000);
